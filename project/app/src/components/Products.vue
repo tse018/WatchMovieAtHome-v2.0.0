e0e562246a124d99bf80c44818b24983a3f2f682
tse018 <60438717+tse018@users.noreply.github.com>
2022-05-25 20:04:15 +0200

    mobile friendly products section

--- project/app/src/components/Products.vue
+++ project/app/src/components/Products.vue
@@ -3,9 +3,22 @@
 
    <div v-else>
       <section class="products-container">
-         <figure class="products-container__product" v-for="product in products">
-            <RouterLink class="products-container__product-link" :to="{name: 'productPage', params: { movie_slug: product.slug.current } }">
-               <img class="products-container__product-image" :src="product.image.asset.url" :alt="product.title" />
+         <figure
+            class="products-container__product"
+            v-for="product in products"
+         >
+            <RouterLink
+               class="products-container__product-link"
+               :to="{
+                  name: 'productPage',
+                  params: { movie_slug: product.slug.current },
+               }"
+            >
+               <img
+                  class="products-container__product-image"
+                  :src="product.image.asset.url"
+                  :alt="product.title"
+               />
 
                <figcaption class="products-container__product-title">
                   {{ product.title }}
@@ -16,7 +29,7 @@
                </h2>
             </RouterLink>
 
-            <button class="product-container__product-button">
+            <button class="products-container__product-button">
                Add to cart
             </button>
          </figure>
@@ -25,8 +38,8 @@
 </template>
 
 <script>
-import query from '../groq/movies.groq?raw';
-import sanityMixin from '../mixins/sanityMixin.js';
+import query from "../groq/movies.groq?raw";
+import sanityMixin from "../mixins/sanityMixin.js";
 
 export default {
    mixins: [sanityMixin],
@@ -37,11 +50,92 @@ export default {
 
    computed: {
       products() {
-         return this.$store.getters.getMovies
-      }
-   }
-}
+         return this.$store.getters.getMovies;
+      },
+   },
+};
 </script>
 
 <style scoped>
+@media screen and (max-width: 600px) {
+   .products-container {
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      gap: 20px;
+      min-height: 100%;
+      margin-top: 30px;
+   }
+
+   .products-container__product {
+      padding: 20px;
+      min-height: 100px;
+      text-align: center;
+      border: 2px solid red;
+   }
+
+   .products-container__product-image {
+      width: 250px;
+      height: 364px;
+   }
+
+   .products-container__product-title {
+      text-align: left;
+      margin: 10px 0 0 10px;
+      font-size: 20px;
+   }
+
+   .products-container__product-price {
+      text-align: right;
+      font-size: 20px;
+   }
+
+   .products-container__product-button {
+      border: 2px solid greenyellow;
+      width: 100%;
+      padding: 10px;
+      background-color: greenyellow;
+      color: black;
+   }
+}
+
+@media screen and (min-width: 1201px) {
+   .products-container {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      gap: 20px;
+      min-height: 100%;
+      margin-top: 30px;
+   }
+
+   .products-container__product {
+      padding: 20px;
+      min-height: 100px;
+      text-align: center;
+      border: 2px solid red;
+   }
+
+   .products-container__product-image {
+      width: 250px;
+      height: 364px;
+   }
+
+   .products-container__product-title {
+      text-align: left;
+      margin: 10px 0 0 10px;
+      font-size: 20px;
+   }
+
+   .products-container__product-price {
+      text-align: right;
+      font-size: 20px;
+   }
+
+   .products-container__product-button {
+      border: 2px solid greenyellow;
+      width: 100%;
+      padding: 10px;
+      background-color: greenyellow;
+      color: black;
+   }
+}
 </style>
--- project/app/src/modules/cart.js
+++ project/app/src/modules/cart.js
@@ -2,7 +2,60 @@ export default {
    state() {
       return {
          cart: [],
-         totalAmount: 0
+         totalAmount: 0,
+         localStorageName: 'Cart' 
       }
+   },
+
+   mutations: {
+      addMovieToCart(state, movie) {
+         let checkIfMovieExist = state.cart.findIndex(product => product.title == movie.title);
+
+         if(checkIfMovieExist !== -1) {
+            state.cart[checkIfMovieExist].quantity += 1;
+         } else {
+            const movieWithQuantity = {
+               ...movie,
+                  quantity: 1
+            }
+            state.cart.push(movieWithQuantity);
+         }
+      },
+
+      removeMovieFromCart(state, movie) {
+         state.cart.findIndex
+      },
+
+      increaseQuantity(state, index) {
+         if(state.cart[index]) {
+            state.cart[index].quantity += 1;
+         }
+      },
+
+      decreaseQuantity(state, index)  {
+         if (state.cart.[index] && state.cart[index].quantity > 0) {
+            state.cart[index].quantity -= 1;
+         }
+      },
+
+      setMovies(state, movies) {
+         state.cart = movies;
+      },
+   },
+
+   actions: {
+      addToCart({commit, dispatch}, movie) {
+         commit('addMovieToCart', movie);
+         dispatch('setToLocalStorage');
+      },
+
+      removeFromCart({commit, dispatch }, itemInCart) {
+         commit('removeMovieFromCart', itemInCart);
+         dispatch('setToLocalStorage');
+      },
+
+      setToLocalStorage({state}) {
+         window.localStorage.setItem(state.localStorageName, JSON.stringify(state.cart));
+      },
    }
 }
