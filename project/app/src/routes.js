330761e9a89628a2828b696b810edd2914810963
tse018 <60438717+tse018@users.noreply.github.com>
2022-05-26 16:29:50 +0200

    route error handling

--- project/app/src/routes.js
+++ project/app/src/routes.js
@@ -1,9 +1,12 @@
 import Home from './views/Home.vue';
 import ProductPage from './views/ProductPage.vue';
 import CartPage from './views/CartPage.vue';
+import PageNotFound from './views/PageNotFound.vue';
 
 export default [
-      { name: 'productPage',  path: '/product/:movie_slug', component: ProductPage },
-      { name: 'cart',         path: '/cart',                component: CartPage }, 
-      { name: 'home',         path: '/',                    component:  Home},
+      {                             path: '/product',                   redirect: to => { return { path: '/',  }}},
+      { name: 'productPage',        path: '/product/:movie_slug',       component: ProductPage },
+      { name: 'cart',               path: '/cart',                      component: CartPage }, 
+      { name: 'home',               path: '/',                          component:  Home},
+      { name: 'pagenotfound',       path: '/:pathMatch(.*)*',           component: PageNotFound },
 ]
