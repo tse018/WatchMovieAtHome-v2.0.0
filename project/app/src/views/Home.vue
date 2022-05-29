f4e26f2b1e7347221d804e49892a6793b10e4b9d
tse018 <60438717+tse018@users.noreply.github.com>
2022-05-29 14:21:27 +0200

    responsive product page

--- project/app/src/views/Home.vue
+++ project/app/src/views/Home.vue
@@ -107,6 +107,49 @@ export default {
    }
 }
 
+
+@media screen and (min-width: 601px) and (max-width: 1200px) {
+   .products-container {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 20px;
+      min-height: 100%;
+      margin-top: 30px;
+   }
+
+   .products-container__product {
+      padding: 20px;
+      min-height: 100px;
+      text-align: center;
+      border: 2px solid red;
+   }
+
+   .products-container__product-image {
+      width: 250px;
+      height: 364px;
+   }
+
+   .products-container__product-title {
+      text-align: left;
+      margin: 10px 0 0 10px;
+      font-size: 20px;
+   }
+
+   .products-container__product-price {
+      text-align: right;
+      font-size: 20px;
+   }
+
+   .products-container__product-button {
+      border: 2px solid greenyellow;
+      width: 100%;
+      padding: 10px;
+      background-color: greenyellow;
+      color: black;
+   }
+}
+
+
 @media screen and (min-width: 1201px) {
    .products-container {
       display: grid;
